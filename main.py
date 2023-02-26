c6d62c7f9a90d0e6825cfc8b1e160816a36f6b33
Yodaman07 <ayaan.irshad@gmail.com>
2023-02-26 15:07:55 -0800

    Fixed Text Formatting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import math
 
 
 # print(os.listdir(os.path.curdir))
@@ -22,34 +23,34 @@ class LineUtil:
             files = returnFiles
 
         fileCount = len(files)
-        possibleFullRows = fileCount // self.rowCount
+        columns = math.ceil(fileCount / self.rowCount)
         remainderColumn = fileCount % self.rowCount
-
-        if remainderColumn == 0:
-            columnCount = possibleFullRows
-        elif remainderColumn > 0:
-            columnCount = possibleFullRows + 1
-        else:
-            columnCount = 0
-
-        for num in range(self.rowCount):
-            l = [f"{num + 1}. {files[num]}"]
-            try:
-                for i in range(possibleFullRows):  # loops over every complete column
-                    if possibleFullRows - 1 > i:
-                        l.append(f"{num + self.rowCount + 1 + i}. {files[num + self.rowCount + i]}")
-                    # elif num == i:
-                    #     l.append(f"{num + self.rowCount + 2 + i}. {files[num + self.rowCount + i]}")
-
-                print("          ".join(l))
-            except IndexError:
-                print("".join(l))
-                # prints item in column without anything in line
+        self.display_formatted_text(files, columns, remainderColumn)
 
     # def downLevel(self):
     #     down = os.listdir("/..")
     #     print(down)
+    def display_formatted_text(self, file_list, c_count, c_remainder):
+        print(len(file_list))
+        length_list = file_list.copy()
+        length_list.sort(reverse=True, key=len)
+        max_length = len(length_list[0])
+        for row in range(self.rowCount):
+            files = []
+            if (row + 1 <= c_remainder) or (c_remainder == 0):  # checks for full row
+                self.add_file(c_count, row, file_list, max_length, files)
+            else:
+                self.add_file(c_count + 1, row, file_list, max_length, files)
+            print("         ".join(files))
+
+    def add_file(self, c_count, row, file_list, max_length, files):
+        for column in range(c_count):
+            index = column * self.rowCount + row
+            file = file_list[index]
+            while len(file) < max_length:
+                file += " "
+            files.append(f"{index + 1}. {file}")
 
 
-util = LineUtil(".", False, 4)
+util = LineUtil(".", False, 6)
 util.upLevel()
